d159044188d093524e8d113ce1c0b018ede5f176
RangerMauve <RangerMauve@htomail.com>
2014-09-07 00:03:43 -0400

    Using fork of mespeak, added voice configs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var speech = require("mespeak");
 var options = require("./options.js");
-
-var voice = require("mespeak/voices/en/en-us.json");
+console.log(speech);
+var voice = "en-us.json";
+speech.loadConfig("mespeak_config.json");
 speech.loadVoice(voice);
 
 options.on("change", function(data) {
