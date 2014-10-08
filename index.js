190445326a0d478eec242dc62086dcbab54a36c0
RangerMauve <RangerMauve@hotmail.com>
2014-10-07 23:00:28 -0400

    Added RTC stuff, not working yet

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var speech = require("mespeak");
 var options = require("./options.js");
 var sounds = require("./sounds.js");
+var tosay = document.getElementById("tosay");
+
+var socket = io.connect("172.20.10.7:8080");
+
+socket.on("speaking", function(data) {
+	var opts = data.voice;
+	var message = data.message;
+})
+
+tosay.addEventListener("submit", function(e) {
+	e.preventDefault();
+	var data = tosay.text.value;
+	var opts = options.getOptions();
+	say(data, opts);
+	socket.emit("speak", {
+		voice: opts,
+		message: data
+	})
+});
 
 speech.loadConfig(require("mespeak/src/mespeak_config.json"));
 speech.loadVoice(require("mespeak/voices/en/en-us.json"));
@@ -10,8 +29,8 @@ options.on("change", function(data) {
 	say("This is your new voice", data);
 });
 
-function say(message,opts){
+function say(message, opts) {
 	opts.rawdata = true;
-	var data = speech.speak(message,opts);
+	var data = speech.speak(message, opts);
 	sounds.play(data);
 }
